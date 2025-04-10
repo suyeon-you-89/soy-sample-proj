fa71640b18a9a90f43c2c6bd98e608ebc191b3de
suyeon-you <suyeon.you.89@gmail.com>
2025-04-11 00:47:29 +0900

    smaple-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
  import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css' 
-import 'soy-admin-ui/soy-admin-ui.css'; 
-import { SampleButton } from 'soy-admin-ui' 
+import 'soy-sample-ui/soy-sample-ui.css'; 
+import { SampleButton } from 'soy-sample-ui' 
 import { useState } from 'react';
 
 function App() {
